069a34857bac8f9de411011fc5526a32d94336cd
pchintalapudi <premc@mit.edu>
2021-07-25 21:43:50 -0700

    New icons

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -1,22 +1,34 @@
 <template>
     <article id="summary">
         <section>
-            <h2>Skills and Tooling</h2>
+            <a class="linkable">
+                <b class="icon"><tools :color="'blue'"></tools></b>
+                <h2>Skills and Tooling</h2>
+            </a>
             <numbered-stat :name="'Coding Languages'" :number="7" :subtext="'C++, Java, C, Python, Typescript, Javascript, MATLAB'"></numbered-stat>
             <numbered-stat :name="'Focus Areas'" :number="3" :subtext="'Intuitive Interfaces, Robust System Design, High Performance Standards'"></numbered-stat>
         </section>
         <section>
-            <h2>Education and Coursework</h2>
+            <a class="linkable">
+                <b class="icon"><graduation-cap :color="'green'"></graduation-cap></b>
+                <h2>Education and Coursework</h2>
+            </a>
             <numbered-stat :name="'Undergraduate Majors'" :number="2" :subtext="'Computer Science Engineering and Bioengineering'"></numbered-stat>
-            <numbered-stat :name="'Classes'" :number="34" :subtext="'Design, Engineering, Maths, Sciences'"></numbered-stat>
+            <numbered-stat :name="'Classes'" :number="34" :subtext="'Engineering, Science, Math, Design'"></numbered-stat>
         </section>
         <section>
-            <h2>Internships and Research</h2>
+            <a class="linkable">
+                <b class="icon"><flask :color="'yellow'"></flask></b>
+                <h2>Internships and Research</h2>
+            </a>
             <numbered-stat :name="'Internships'" :number="4" :subtext="'NVIDIA, doc.ai, McAfee, Sandia National Labs'"></numbered-stat>
             <numbered-stat :name="'Research Groups'" :number="4" :subtext="'Gertler Lab, Sandia National Labs, Blainey Lab, JuliaLab'"></numbered-stat>
         </section>
         <section>
-            <h2>Personal Projects</h2>
+            <a class="linkable">
+                <b class="icon"><monitor :color="'red'"></monitor></b>
+                <h2>Personal Projects</h2>
+            </a>
             <numbered-stat :name="'Major Projects'" :number="6" :subtext="'Long Project Lifetimes and Large Feature Sets'"></numbered-stat>
             <numbered-stat :name="'Websites'" :number="3" :subtext="'MakeMIT 2019 & 2020, LMF 2020-present, This Website'"></numbered-stat>
         </section>
@@ -25,10 +37,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import NumberedStat from './subcomponents/NumberedStat.vue';
+import GraduationCap from './icons/GraduationCap.vue';
+import Tools from './icons/Tools.vue';
+import Monitor from './icons/Monitor.vue';
+import Flask from './icons/Flask.vue';
 
 export default defineComponent({
     components: {
-        NumberedStat
+        NumberedStat,
+        GraduationCap,
+        Tools,
+        Monitor,
+        Flask
     },
     setup() {
         
@@ -45,5 +65,29 @@ export default defineComponent({
 #summary > * {
     flex: 1;
     text-align: center;
+    align-items: center;
+}
+.icon {
+    width: 100px;
+    height: 100px;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    margin: 20px;
+}
+.icon > * {
+    transform: scale(2.5);
+}
+.icon::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(var(--fg-color), 0.1);
+    border-radius: 50%;
+}
+.linkable {
+    cursor: pointer;
+    align-items: center;
 }
 </style>
---
+++ src/components/icons/GraduationCap.vue
@@ -0,0 +1,40 @@
+<template>
+    <i :class="['graduation-cap', color]"></i>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  props: {
+    color: String
+  }
+});
+</script>
+<style scoped>
+.graduation-cap::before {
+  position: absolute;
+  content: "";
+  background-color: var(--color);
+  transition: background-color 300ms;
+  height: 10px;
+  width: 19.5px;
+  transform: translate(10px, 10px);
+  border-bottom-left-radius: 5px 2px;
+  border-bottom-right-radius: 5px 2px;
+}
+.graduation-cap::after {
+  position: absolute;
+  content: "";
+  height: 10px;
+  width: 20px;
+  background-color: var(--color);
+  box-shadow: -7.5px 5.5px 0 0px rgb(var(--bg-color));
+  transition: background-color 300ms, box-shadow 300ms;
+  transform: translate(10px, 0px) rotate(-10deg) skewX(60deg)
+    rotate3d(1, 0, 0, 45deg);
+}
+.graduation-cap {
+  position: relative;
+  width: 40px;
+  height: 20px;
+}
+</style>
