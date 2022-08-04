3b454da32f3d3c0efd20f7dbc69f83d657b8e21d
Prem Chintalapudi <prem.chintalapudi@gmail.com>
2022-08-03 17:56:50 -0700

    Define store interfaces

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,40 @@
 import { createStore } from 'vuex'
 
+export interface Class {
+  name: string;
+  id: string;
+  tags: Array<"cs" | "bio">;
+}
+
+export interface Work {
+  id: string;
+  company: string;
+  role: string;
+  start: string;
+  end: string;
+  description: string;
+  tags: Array<"cs" | "bio" | "research" | "internship">;
+  supervisors: Array<{ name: string, title: string }>;
+  hidden?:boolean;
+}
+
+export interface Project {
+  id: string;
+  name: string;
+  description: string;
+  tags: string[];
+  start: string;
+  end: string;
+  github?: string;
+  link?: string;
+}
+
+export interface State {
+  courses: Class[];
+  work: Work[];
+  projects: Project[];
+}
+
 export default createStore({
   state: {
   },
