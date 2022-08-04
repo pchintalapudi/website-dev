54ff6ff65c6797d80bd367f410d806e23a505f21
Prem Chintalapudi <prem.chintalapudi@gmail.com>
2022-08-03 17:56:37 -0700

    Update dependencies and build files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.module
+      .rule('yaml')
+        .test(/\.ya?ml?$/)
+        .use('json-loader')
+          .loader('json-loader')
+          .end()
+        .use('js-yaml-loader')
+          .loader('js-yaml-loader')
+  },
+  outputDir: path.resolve(__dirname, "docs"),
+  assetsDir: "static"
 })
